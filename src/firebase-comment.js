fabf2c920b87bbdb8961c11a5cb780d8107b550a
Aryaa0112-A <aryadillah0112@gmail.com>
2025-02-06 04:38:44 +0700

    Update fitur portfolio dan perbaikan bug

--- src/firebase-comment.js
+++ src/firebase-comment.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCs6jeDaoRyKn5xWRBVfzMr1wdskSE-HFM",
     authDomain: "portofolio-62d18.firebaseapp.com",
     projectId: "portofolio-62d18",
-    storageBucket: "web-kelas-tes.appspot.com",
+    storageBucket: "portofolio-62d18.firebasestorage.app",
     messagingSenderId: "163461699037",
     appId: "1:163461699037:web:15789bd2b5ed9a88e35234"
 };
